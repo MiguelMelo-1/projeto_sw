da36b5d7621fdc4b247f08fafb5a8a36cbf334dd
Eggo4 <luismnunesferreira@gmail.com>
2023-05-30 19:57:34 +0100

    New

--- project/__init__.py
+++ project/__init__.py
@@ -7,7 +7,6 @@ from project.forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e6e7367df57e95a4788360c842521e3e'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
-
 db = SQLAlchemy(app)
 
 bcrypt = Bcrypt(app)
