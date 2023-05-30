9af7ff5f76327b03dd29de0539625c3936f40a5f
MiguelMelo-1 <pipocamiguel@gmail.com>
2023-05-30 23:22:51 +0100

    MiguelMelo

--- project/models.py
+++ project/models.py
@@ -1,11 +1,13 @@
 from project import db, login_manager
 from datetime import datetime
+from flask_login import UserMixin
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(30), nullable=False, unique=True)
     email_address = db.Column(db.String(50),nullable=False, unique=True)
