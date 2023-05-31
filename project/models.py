9ed74410216378a95ed6afedc947c4e227a67441
MiguelMelo-1 <pipocamiguel@gmail.com>
2023-05-31 12:09:12 +0100

    awd

--- project/models.py
+++ project/models.py
@@ -12,11 +12,9 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(30), nullable=False, unique=True)
     email_address = db.Column(db.String(50),nullable=False, unique=True)
     password_hash = db.Column(db.String(60),nullable=False)
-    
     # @property
     # def password(self):
     #     return self.password
-    
     # @password.setter
     # def password(self, plain_text_password):
     #     self.password_hash =  bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
@@ -32,3 +30,5 @@ class Task(db.Model):
     date_start = db.Column(db.String(), default=date_time.strftime("%Y-%m-%d"))
     date_end = db.Column(db.String())
     id_user = db.Column(db.Integer(), db.ForeignKey('user.id'), nullable=True)
+    # def __repr__(self):
+    #     return '{id = %r, title = %r, desc = %r, category = %r, state = %r, date_start = %r, date_end = %r, id_user = %r}' % self.id % self.title % self.desc % self.category % self.state % self.date_start % self.date_end % self.id_user
